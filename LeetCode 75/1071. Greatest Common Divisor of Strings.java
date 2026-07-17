f2de04febd5639eab22066d23e537d20e42a0df4
zokacode <114281352+zokacode@users.noreply.github.com>
2026-07-17 16:20:26 +0800

    Update 1071. Greatest Common Divisor of Strings.java

--- LeetCode 75/1071. Greatest Common Divisor of Strings.java
+++ LeetCode 75/1071. Greatest Common Divisor of Strings.java
@@ -1,22 +1,14 @@
 class Solution {
     public String gcdOfStrings(String str1, String str2) {
-
-        if (str1.equals(str2)) {
-            return str1;
-        }
-
-        if (str1.length() > str2.length()) {
-            if (!str1.startsWith(str2)) {
-                return "";
+        while (!str1.equals(str2)) {
+            if (str1.length() > str2.length()) {
+                if (!str1.startsWith(str2)) return "";
+                str1 = str1.substring(str2.length());
             } else {
-                return gcdOfStrings(str1.substring(str2.length()), str2);
-            }
-        } else {
-            if (!str2.startsWith(str1)) {
-                return "";
-            } else {
-                return gcdOfStrings(str1, str2.substring(str1.length()));
+                if (!str2.startsWith(str1)) return "";
+                str2 = str2.substring(str1.length());
             }
         }
+        return str1;
     }
 }
